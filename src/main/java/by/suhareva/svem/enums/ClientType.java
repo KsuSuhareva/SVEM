67d1e6109a55042276e9056bcecb72b68865b2f3
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-02-28 14:09:11 +0300

    создала enum ClientType.java и StatusInBase.java

---
+++ src/main/java/by/suhareva/svem/enums/ClientType.java
@@ -0,0 +1,6 @@
+package by.suhareva.svem.enums;
+
+public enum ClientType {
+    LEGAL_ENTITY,
+    INDIVIDUAL;
+}
---
+++ src/main/java/by/suhareva/svem/enums/StatusInBase.java
@@ -0,0 +1,7 @@
+package by.suhareva.svem.enums;
+
+public enum StatusInBase {
+    NEW,
+    DELETE,
+    DEFAULT;
+}
