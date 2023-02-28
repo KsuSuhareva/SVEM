02c2187a9174abbcb0a0dca3fdcdb7c1bb189b9b
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-02-28 12:51:32 +0300

    Init commit

---
+++ src/main/java/by/suhareva/svem/SvemApplication.java
@@ -0,0 +1,13 @@
+package by.suhareva.svem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/suhareva/svem/SvemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.suhareva.svem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
