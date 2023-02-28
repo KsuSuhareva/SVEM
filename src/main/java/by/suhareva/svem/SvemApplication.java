01af79e38592c4515dc4511e87cb071dbac4853a
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-02-28 13:01:19 +0300

    создала ветку develop, добавила scanBasePackages

--- src/main/java/by/suhareva/svem/SvemApplication.java
+++ src/main/java/by/suhareva/svem/SvemApplication.java
@@ -3,7 +3,7 @@ package by.suhareva.svem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages =  "by.suhareva.svem")
 public class SvemApplication {
 
     public static void main(String[] args) {
