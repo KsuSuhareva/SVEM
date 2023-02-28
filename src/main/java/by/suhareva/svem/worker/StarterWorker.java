0578ab0b028063bc0fa5050b34342b0dd5d4a3f6
Kseniya Suhareva <suharevaksu86@gmail.com>
2023-02-28 14:13:27 +0300

    создала поток Worker.java, который работает с очередью запросов и ответов

---
+++ src/main/java/by/suhareva/svem/worker/StarterWorker.java
@@ -0,0 +1,40 @@
+package by.suhareva.svem.worker;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StarterWorker {
+
+    private final Worker worker;
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    @PostConstruct
+    public void start() {
+        log.info("Worker started");
+        executorService.submit(worker);
+        try {
+            executorService.awaitTermination(500, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    @PreDestroy
+    public void close() {
+        executorService.shutdown();
+        log.info("Worker close");
+    }
+
+
+}
---
+++ src/main/java/by/suhareva/svem/worker/Worker.java
@@ -0,0 +1,90 @@
+package by.suhareva.svem.worker;
+
+import by.suhareva.svem.db.model.Fine;
+import by.suhareva.svem.db.model.GetRequest;
+import by.suhareva.svem.db.model.SendResponse;
+import by.suhareva.svem.exceptions.DAOException;
+import by.suhareva.svem.repositories.FineRepository;
+import by.suhareva.svem.repositories.RequestRepository;
+import by.suhareva.svem.repositories.ResponseRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+
+import static by.suhareva.svem.enums.StatusInBase.NEW;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@Getter
+public class Worker implements Runnable {
+
+    private final RequestRepository requestRepository;
+    private final FineRepository fineRepository;
+    private final ResponseRepository responseRepository;
+
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        while (true) {
+            try {
+                Optional<GetRequest> optionalRequest = requestRepository.getByMinDate();
+                if (optionalRequest.isPresent()) {
+                    GetRequest request = optionalRequest.get();
+                    Optional<Fine> fine = fineRepository.getByNumber(request.getNumber());
+
+                    SendResponse response = getResponse(optionalRequest, fine);
+                    responseRepository.save(response);
+
+                    requestRepository.delete(request.getUuid());
+                }
+                Thread.sleep(100);
+            } catch (DAOException e) {
+                log.error("DAOException with cause: {}", e.getMessage());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private SendResponse getResponse(Optional<GetRequest> request, Optional<Fine> fine) {
+        if (fine.isPresent()) {
+            return createResponse(request, fine);
+        } else {
+            return createResponse(request);
+        }
+    }
+
+    private SendResponse createResponse(Optional<GetRequest> optionalRequest, Optional<Fine> optionalFine) {
+        GetRequest request = optionalRequest.get();
+        Fine fine = optionalFine.get();
+        SendResponse response = SendResponse.builder()
+                .uuid(UUID.randomUUID())
+                .uuid_request(request.getUuid())
+                .id_fine(fine.getId())
+                .number(fine.getNumber())
+                .resolution_num(fine.getResolution_num())
+                .resolution_date(fine.getResolution_date())
+                .accrued(fine.getAccrued())
+                .paid(fine.getPaid())
+                .date(new Date())
+                .status(NEW).build();
+        return response;
+    }
+
+    private SendResponse createResponse(Optional<GetRequest> optionalRequest) {
+        GetRequest request = optionalRequest.get();
+        SendResponse response = SendResponse.builder()
+                .uuid(UUID.randomUUID())
+                .uuid_request(request.getUuid())
+                .date(new Date())
+                .status(NEW).build();
+        return response;
+    }
+}
